a9738a3264abae96c7e1fc0d73140be62eb69d1e
liqwang <liqwang2@gmail.com>
2025-05-03 14:12:13 +0800

    chore(staker): change the deadline from 30s to 72h

--- 1-staker/packages/nextjs/contracts/deployedContracts.ts
+++ 1-staker/packages/nextjs/contracts/deployedContracts.ts
@@ -33,7 +33,7 @@ const deployedContracts = {
       inheritedFunctions: {},
     },
     Staker: {
-      address: "0xc3e53F4d16Ae77Db1c982e75a937B9f60FE63690",
+      address: "0x84eA74d481Ee0A5332c457a4d796187F6Ba67fEB",
       abi: [
         {
           inputs: [
