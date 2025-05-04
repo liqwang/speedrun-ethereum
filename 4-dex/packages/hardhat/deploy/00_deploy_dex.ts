e52973fe5044c51c550ee521b48b0080426977f2
liqwang <liqwang2@gmail.com>
2025-05-05 01:24:48 +0800

    release(dex): deploy to Sepolia testnet https://sepolia.etherscan.io/address/0x85147b6d0260e5C326B5Ce654B9b4272e2C625f0 https://speedrunethereum-4-dex.vercel.app

--- 4-dex/packages/hardhat/deploy/00_deploy_dex.ts
+++ 4-dex/packages/hardhat/deploy/00_deploy_dex.ts
@@ -63,8 +63,8 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   // If you are going to the testnet make sure your deployer account has enough ETH
   await balloons.approve(dexAddress, hre.ethers.parseEther("100"));
   console.log("INIT exchange...");
-  await dex.init(hre.ethers.parseEther("1"), {
-    value: hre.ethers.parseEther("1"),
+  await dex.init(hre.ethers.parseEther("0.1"), {
+    value: hre.ethers.parseEther("0.1"),
     gasLimit: 200000,
   });
 };
