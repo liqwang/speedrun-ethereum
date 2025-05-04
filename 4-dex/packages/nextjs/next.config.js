006c9dd942b75c03da369ad84f7016de305d0b24
liqwang <liqwang2@gmail.com>
2025-05-04 12:52:45 +0800

    feat(dex): init challenge #4: DEX https://speedrunethereum.com/challenge/minimum-viable-exchange

---
+++ 4-dex/packages/nextjs/next.config.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
+
+const isIpfs = process.env.NEXT_PUBLIC_IPFS_BUILD === "true";
+
+if (isIpfs) {
+  nextConfig.output = "export";
+  nextConfig.trailingSlash = true;
+  nextConfig.images = {
+    unoptimized: true,
+  };
+}
+
+module.exports = nextConfig;
